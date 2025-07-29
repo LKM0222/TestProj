415278b9ef0453c96ddd4f35b8b7f6f71c371c12
LKM0222 <python03@naver.com>
2025-07-29 16:59:39 +0900

    Jenkins init

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+class MyEditorScript
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static void PerformBuild()
+    {
+        BuildPipeline.BuildPlayer(FindEnabledEditorScenes(), "Builds/Windows/MyGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+
+        return EditorScenes.ToArray();
+    }
+}
